e495e5154791930543358d9717a88b56f49d8b23
siladigy <43274452+siladigy@users.noreply.github.com>
2018-11-27 12:16:08 +0200

    js additional second

---
+++ js-add3/script.js
@@ -0,0 +1,9 @@
+nextPrime:
+  for (var i = 2; i <= 100; i++) {
+
+    for (var j = 2; j < i; j++) {
+      if (i % j == 0) continue nextPrime;
+    }
+
+    console.log(i, 'делитель этого числа 1 и ' +i);
+  }
